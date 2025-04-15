6e49cc35371dc5c7a5aeb3800d1f8b59dcc7db98
Shubham Chourasia <bhopalshubh@gmail.com>
2025-04-15 16:58:27 +0530

    ..

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -19,7 +19,7 @@ export default function BlogCard({ post }: BlogProps) {
  
   
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 shadow-sm sm:grid-cols-2 lg:grid-cols-3 gap-6">
       <div className="card text-base-content w-96 ">
         <figure className="h-56 rounded-4xl">
           <img
